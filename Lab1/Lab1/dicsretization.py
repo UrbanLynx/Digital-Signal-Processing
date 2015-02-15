c51801c1c7d902eb98f7aa2ad7fa296113d3118e
Stanislav <stasmushits@gmail.com>
2015-02-15 22:58:32 +0300

    renamed variables and fields, added documentation

--- Lab1/Lab1/dicsretization.py
+++ Lab1/Lab1/dicsretization.py
@@ -3,26 +3,36 @@ from digital_signals import *
 
 def main():
     imp_sig = ImpulseSignal()
-    discretize_impulse(imp_sig)
+    discretize_signal(imp_sig)
 
     gaus_sig = GaussianSignal()
-    discretize_impulse(gaus_sig)
+    discretize_signal(gaus_sig)
 
     plot_signal((imp_sig,gaus_sig))
 
 
-def discretize_impulse(sg):
-    signal_vectorized = vectorize(sg.signal)
+def discretize_signal(sig):
+    """
+    Calculate ideal and reconstructed signals
+
+    sig - DigitalSignal object
+    """
+    signal_vectorized = vectorize(sig.signal)
     kotelnikov_vectorized = vectorize(kotelnikov)
 
-    sg.x_sample = linspace(-sg.global_limit, sg.global_limit, sg.ideal_samples)
-    sg.y_sample = signal_vectorized(sg.x_sample)
+    sig.x_sample = linspace(-sig.global_limit, sig.global_limit, sig.ideal_samples)
+    sig.y_sample = signal_vectorized(sig.x_sample)
 
-    sg.x_recon = linspace(-sg.global_limit, sg.global_limit, sg.num_samples)
-    sg.y_recon = kotelnikov_vectorized(sg,sg.x_recon)
+    sig.x_recon = linspace(-sig.global_limit, sig.global_limit, sig.recon_samples)
+    sig.y_recon = kotelnikov_vectorized(sig,sig.x_recon)
 
 
 def kotelnikov(sig, t):
+    """
+    Reconstruct Y for argument t and signal sig
+
+    sig - DigitalSignal object
+    """
     return sum([sig.signal(k*sig.dt) * my_sinc(math.pi/sig.dt*(t-k*sig.dt))
                 for k in range(-sig.k_limit, sig.k_limit, 1)])
 
@@ -32,6 +42,11 @@ def my_sinc(x):
 
 
 def plot_signal(signals):
+    """
+    Plots signals
+
+    signals - list of DigitalSignal objects for plotting
+    """
     num = len(signals)
     fig, axes = plt.subplots(1, num)
     for axis, sg in zip(axes, signals):
--- Lab1/Lab1/digital_signals.py
+++ Lab1/Lab1/digital_signals.py
@@ -2,17 +2,35 @@ import math
 
 
 class DigitalSignal:
-    x_sample, y_sample = None, None
+    """
+    Class represents settings for discretization of a signal
+
+    x_ideal, y_ideal - points for ideal signal
+    ideal_samples - number of points in ideal signal
+    x_recon, y_recon - points for reconstructed signal
+    recon_samples - number of points in reconstructed signal
+    dt - distance between points in reconstructed signal
+    global_limit - left and right limits of the signal on plot
+    title - title of plot
+    k_limit - number of points of ideal signal considered during reconstructing of one point of reconstructed signal
+    """
+
+    x_ideal, y_ideal = None, None
+    ideal_samples = 1000
     x_recon, y_recon = None, None
+    recon_samples = 500
     dt = 0.01
-    num_samples = 500
-    global_limit = num_samples * dt/2
+    global_limit = recon_samples * dt/2
     title = 'signal'
     k_limit = 1000
-    ideal_samples = 1000
 
 
 class ImpulseSignal(DigitalSignal):
+    """
+    impulse_limit - left and right boundaries of an impulse signal
+    amplitude - amplitude of a signal
+    """
+
     impulse_limit = 1
     amplitude = 1
 
@@ -24,6 +42,11 @@ class ImpulseSignal(DigitalSignal):
 
 
 class GaussianSignal(DigitalSignal):
+    """
+    sigma - standard deviation of a signal
+    amplitude - amplitude of a signal
+    """
+
     sigma = 0.7
     amplitude = 1
 
